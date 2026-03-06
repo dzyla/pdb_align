7c534f85d88a2b7e8fb91e7f46efbd1f445e18cc
Dawid Zyla <didny1@gmail.com>
2026-03-06 12:07:09 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pdb_align",
+    version="0.1.0",
+    description="Object-oriented protein structure alignment package.",
+    packages=find_packages(),
+    install_requires=[
+        "numpy<2.0",
+        "pandas",
+        "scikit-learn",
+        "scipy",
+        "biopython",
+        "py3Dmol",
+        "stmol",
+        "plotly",
+        "ipython_genutils",
+        "matplotlib",
+        "streamlit",
+        "streamlit-molstar"
+    ],
+)
