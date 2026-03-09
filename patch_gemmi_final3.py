4ed1d41533b29b6f8cca25ddf18aec195ff2d7e7
Dawid Zyla <didny1@gmail.com>
2026-03-09 14:31:38 -0700

    cleanup

--- patch_gemmi_final3.py
+++
@@ -1,8 +0,0 @@
-import re
-
-with open("pdb_align/core.py", "r") as f:
-    text = f.read()
-
-# Make sure the selector works with gemmi chain names
-# In `_parse_chain_selector`, it returns `chain.name`. But we need to make sure we're querying correctly.
-# In `def _resolve_selectors`, we need to see what `sel` is.
--- test_metrics.py
+++
@@ -1,2 +0,0 @@
-import pdb_align.metrics as metrics
-print(metrics.calculate_tm_pvalue(0.5, 100))
